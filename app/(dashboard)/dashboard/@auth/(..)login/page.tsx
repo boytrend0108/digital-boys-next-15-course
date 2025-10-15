bb060cf836fa6582e24cacc6d4436ea26ff55ebb
boytrend0108 <boytrend@gmail.com>
2025-10-15 08:23:22 +0300

    feat: implement intercrpt routes

---
+++ app/(dashboard)/dashboard/@auth/(..)login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Modallogin() {
+  return (
+    <div className='fixed h-full w-full bg-amber-800 opacity-50 top-0 start-0 flex justify-center items-center'>
+      Modal login
+    </div>
+  );
+}
